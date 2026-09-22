797ad16c882e72306148d1c51b28fbbcda7fd62f
ashish a jaison <88699229+ashish-a-jaison@users.noreply.github.com>
2026-09-22 12:47:52 +0530

    Time: 1 ms (28.97%), Space: 55.2 MB (12.07%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    for(i=1;i*i<=num;i++){
+        if(i*i===num){
+            return true
+        } 
+        }
+      return false
+};
